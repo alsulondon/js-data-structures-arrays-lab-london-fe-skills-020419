7b464ede3496141f080d948fa98a862ae47701b6
Alsu Chalikova <alsu.halikova@gmail.com>
2019-03-13 20:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const drivers=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift(name);
+}
+function appendDriver(name) {
+  const newDrivers= [...drivers, name];
+  return newDrivers;
+}
+
+
+function prependDriver(name) {
+  const newDrivers= [Ralph]
+  return newDrivers;
+}
+
+function removeLastDriver(name){
+  drivers.shift(3);
+  return drivers;
+}
+  
+
